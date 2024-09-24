5873d2e1ec12e0fc31e38b15bff72ea6640b4f6d
happyresh <elinielvrecio@iskolarngbayan.pup.edu.ph>
2024-09-24 13:44:35 +0800

    update

--- DataCamp Python/introduction.py
+++ DataCamp Python/introduction.py
@@ -15,3 +15,13 @@ savings = 100
 
 # Print out savings
 print(savings)
+
+# Create the variables monthly_savings and num_months
+monthly_savings = 10
+num_months = 4
+
+# Multiply monthly_savings and num_months
+new_savings = (monthly_savings * num_months)
+
+# Print new_savings
+print (new_savings)
