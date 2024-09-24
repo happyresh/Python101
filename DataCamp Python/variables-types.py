4dc03c6128ad38fb08ab3846f96cc160ae4b527b
happyresh <elinielvrecio@iskolarngbayan.pup.edu.ph>
2024-09-24 13:51:54 +0800

    update

---
+++ DataCamp Python/variables-types.py
@@ -0,0 +1,46 @@
+# Addition
+print (4+5)
+
+# Subtraction
+print (5-5)
+
+# Multiplication
+print (3*5)
+
+# Division
+print (10/2)
+
+# Create a variable savings
+savings = 100
+
+# Print out savings
+print(savings)
+
+# Create the variables monthly_savings and num_months
+monthly_savings = 10
+num_months = 4
+
+# Multiply monthly_savings and num_months
+new_savings = (monthly_savings * num_months)
+
+# Print new_savings
+print (new_savings)
+
+# Create a variable half
+half = 0.5
+
+# Create a variable intro
+intro = 'Hello! How are you?'
+
+# Create a variable is_good
+is_good = True
+
+savings = 100
+new_savings = 40
+
+# Calculate total_savings using savings and new_savings
+total_savings = savings+new_savings
+print(total_savings)
+
+# Print the type of total_savings
+print(type(total_savings))
